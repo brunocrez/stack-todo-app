5294ad5b6fe7d7be6c56d7c0138c044848a50bc7
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-25 18:25:23 -0300

    configuring cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,12 @@
 const PORT = 3003;
 
 const express = require('express');
+const allowCors = require('./cors');
 
 const server = express();
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(allowCors);
 
 server.listen(PORT, () => {
   console.log(`backend listening on port ${PORT}`);

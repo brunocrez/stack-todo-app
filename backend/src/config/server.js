9a9f2d5166b7ad3f6d3a53f9f8cf4c08ef40d75a
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-25 17:42:04 -0300

    init server and services config

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const PORT = 3003;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log(`backend listening on port ${PORT}`);
+})
+

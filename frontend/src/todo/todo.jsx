f7e52ccc836d439774238112c469987018737cb5
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-26 17:04:06 -0300

    creating extra components to distribute functions (grid, if, iconButton)

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,30 @@ import PageHader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 export default class Todo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { description: '', list: [] };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
+  }
+
+  handleAdd() {
+    console.log(this)
+  }
+
   render() {
     return (
       <div>
         <PageHader name="Tarefas" small="Cadastro" />
-        <TodoForm />
+        <TodoForm
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}
+          description={this.state.description} />
         <TodoList />
       </div>
     )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
+
 export default props => (
-  <div>
-    <h1>form</h1>
+  <div role="form" className="todoForm">
+    <Grid cols="12 9 10">
+      <input 
+        type="text" 
+        id="description"
+        value={props.description}
+        onChange={props.handleChange}
+        className="form-control"
+        placeholder="Adicione uma tarefa"/>
+    </Grid>
+
+    <Grid cols="12 3 2">
+      <IconButton 
+        style="primary" 
+        icon="plus"
+        onClick={props.handleAdd} />
+    </Grid>
   </div>
 )

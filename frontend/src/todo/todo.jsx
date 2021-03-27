04c76df2f9e19f6a941d8a7ebe96a43019957195
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-27 10:30:07 -0300

    filling list component and implementing delete method

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import PageHader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
+
+const API = 'http://localhost:3003/api/todos';
 export default class Todo extends Component {
 
   constructor(props) {
     super(props);
     this.state = { description: '', list: [] };
+
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
+  }
+
+  refresh() {
+    axios
+      .get(`${API}?sort=-createdAt`)
+      .then(res => this.setState({ ...this.state, description: '', list: res.data }));
   }
 
   handleChange(e) {
@@ -17,7 +30,16 @@ export default class Todo extends Component {
   }
 
   handleAdd() {
-    console.log(this)
+    const description = this.state.description;
+    axios
+      .post(API, { description })
+      .then(() => this.refresh());
+  }
+
+  handleRemove(item) {
+    axios
+      .delete(`${API}/${item._id}`)
+      .then(() => this.refresh());
   }
 
   render() {
@@ -28,7 +50,9 @@ export default class Todo extends Component {
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
           description={this.state.description} />
-        <TodoList />
+        <TodoList 
+          list={this.state.list}
+          handleRemove={this.handleRemove} />
       </div>
     )
   }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-export default props => (
-  <div>
-    <h1>list</h1>
-  </div>
-)
+import IconButton from '../template/iconButton';
+
+export default props => {
+
+  function renderRows() {
+    const data = props.list || [];
+
+    return data.map(item => (
+      <tr key={item._id}>
+        <td>{item.description}</td>
+        <td>
+          <IconButton
+            style="danger"
+            icon="trash-o"
+            onClick={() => props.handleRemove(item)}>
+          </IconButton>
+        </td>
+      </tr>
+    ));
+  }
+  
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  );
+}

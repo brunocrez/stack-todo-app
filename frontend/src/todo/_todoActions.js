6e4c5b8c97ea56110ac75a8c9707b2656f62d0fd
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-30 10:23:49 -0300

    changing todoForm to class component for willMount usage

--- frontend/src/todo/_todoActions.js
+++ frontend/src/todo/_todoActions.js
@@ -1,7 +1,21 @@
+import axios from 'axios';
+
+const API = 'http://localhost:3003/api/todos';
+
 // Action Creator
 export function changeDescription(e) {
   return {
     type: 'CHANGE_DESCRIPTION',
     payload: e.target.value
+  }
+}
+
+// Action Creator
+export function getAllTodos() {
+  const response = axios.get(`${API}?sort=-createdAt`);
+
+  return {
+    type: 'SEARCH_TODO',
+    payload: response
   }
 }
--- frontend/src/todo/_todoReducer.js
+++ frontend/src/todo/_todoReducer.js
@@ -23,6 +23,8 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case 'CHANGE_DESCRIPTION':
       return { ...state, description: action.payload }
+    case 'SEARCH_TODO':
+      return { ...state, list: action.payload.data }
     default:
       return state
   }    
